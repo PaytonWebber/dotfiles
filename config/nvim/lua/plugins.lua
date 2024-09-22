628c17cc33da515113dd175fd475a7c930b619d4
Payton Webber <paytonwebber@gmail.com>
2024-09-22 10:16:18 -0700

    added luasnip support

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -125,28 +125,50 @@ require("lazy").setup({
 	},
 
 	-- Autocompletion
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		config = function()
-			-- Here is where you configure the autocompletion settings.
-			local lsp_zero = require("lsp-zero")
-			lsp_zero.extend_cmp()
 
-			-- And you can configure cmp even more, if you want to.
-			local cmp = require("cmp")
-			local cmp_action = lsp_zero.cmp_action()
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.3.0",
+    },
 
-			cmp.setup({
-				formatting = lsp_zero.cmp_format(),
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-				}),
-			})
-		end,
-	},
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        config = function()
+            local lsp_zero = require("lsp-zero")
+            lsp_zero.extend_cmp()
+
+            local cmp = require("cmp")
+            local cmp_action = lsp_zero.cmp_action()
+
+            cmp.setup({
+                formatting = lsp_zero.cmp_format(),
+
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-y>"] = cmp.mapping.complete(),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                }),
+            })
+
+            sources = cmp.config.sources({
+                { name = 'luasnip' },
+            }, {
+                { name = 'buffer' },
+            })
+        end,
+    },
 
 	-- LSP
 	{
