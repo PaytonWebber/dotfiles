fa111eb6c56b998b2239f1879076828cf84abdee
Payton Webber <paytonwebber@gmail.com>
2025-03-03 17:00:17 -0800

    update main bar

--- config/eww/scripts/get-workspaces.sh
+++ config/eww/scripts/get-workspaces.sh
@@ -3,41 +3,34 @@
 NUM_WS=5
 
 spaces() {
-  ACTIVE_WS=$(hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id')
+  HYPR_MONITORS=$(hyprctl monitors -j)
+  HYPR_WORKSPACES=$(hyprctl workspaces -j)
 
-  ACTIVE_WORKSPACES=$(hyprctl workspaces -j)
+  ACTIVE_MONITORS=$(echo "$HYPR_MONITORS" | jq '[.[0].activeWorkspace.id, .[1].activeWorkspace.id]')
+  ACTIVE_WORKSPACES=$(echo "$HYPR_WORKSPACES" | jq '[.[] | select(.windows > 0) | .id]')
 
-  json_output="["
-
-  for ((i=1; i<=NUM_WS; i++)); do
-    if [[ "$i" == "$ACTIVE_WS" ]]; then
-      focused=true
-    else
-      focused=false
-    fi
-
-    if echo "$ACTIVE_WORKSPACES" | grep -q "\"id\": $i"; then
-      active=true
-    else
-      active=false
-    fi
-
-    json_output+="{\"id\": \"$i\", \"focused\": $focused, \"active\": $active}"
-
-    if [[ $i -lt $NUM_WS ]]; then
-      json_output+=", "
-    fi
-  done
-
-  json_output+="]"
+  json_output=$(jq -c -n --argjson num_ws "$NUM_WS" --argjson active_monitors "$ACTIVE_MONITORS" --argjson active_workspaces "$ACTIVE_WORKSPACES" '
+  [range(1; $num_ws+1) as $i |
+    {
+      workspace_id: $i,
+      focused: {
+        value: ($i == $active_monitors[0] or $i == $active_monitors[1]),
+        monitor: (if $i == $active_monitors[0] then 0 
+                  elif $i == $active_monitors[1] then 1 
+                  else null end)
+      },
+      active: ($i | IN($active_workspaces[]))
+    }
+  ] // []'
+  )
 
   echo "$json_output"
 }
 
 spaces
-
-# Listen for workspace events and re-run the spaces function on update
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
-  spaces
-done
-
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | 
+  while read -r line; do
+    case "$line" in
+      *"workspace"*) spaces ;;
+    esac
+  done
