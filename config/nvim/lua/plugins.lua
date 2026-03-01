e2438d8a8c9ec515db1b7d5dfdd2901779faa7bf
Payton Webber <paytonwebber@gmail.com>
2026-02-28 22:01:40 -0800

    Fix typo

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -75,7 +75,7 @@ require("lazy").setup({
         config = function(_, opts)
             local lspconfig = require('lspconfig')
             for server, config in pairs(opts.servers) do
-                config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilitites)
+                config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
                 lspconfig[server].setup(config)
             end
         end,
